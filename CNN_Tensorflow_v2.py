0d925ae09b01279c829d4bcfeb2cf0613ea95011
Sahil Rajesh Dhayalkar <sdhayalk@asu.edu>
2017-10-10 14:52:06 -0700

    added xavier initialization, removed dropout

--- CNN_Tensorflow_v2.py
+++ CNN_Tensorflow_v2.py
@@ -47,11 +47,11 @@ y = tf.placeholder(tf.float32, shape=[None, DIM_OUTPUT])
 
 # define the weights with respective shapes
 weights = {
-	'w1': tf.Variable(tf.random_normal([3, 3, 1, 32])),		#[filter_dim_1, filter_dim_2, num_input_channels, num_output_channels]
-	'w2': tf.Variable(tf.random_normal([3, 3, 32, 64])),
-	'w3': tf.Variable(tf.random_normal([3, 3, 64, 128])),
-	'wfc1': tf.Variable(tf.random_normal([7*7*128, 1024])),
-	'wfc2': tf.Variable(tf.random_normal([1024, DIM_OUTPUT])),
+	'w1': tf.Variable(tf.contrib.layers.xavier_initializer_conv2d([3, 3, 1, 32])),		#[filter_dim_1, filter_dim_2, num_input_channels, num_output_channels]
+	'w2': tf.Variable(tf.contrib.layers.xavier_initializer_conv2d([3, 3, 32, 64])),
+	'w3': tf.Variable(tf.contrib.layers.xavier_initializer_conv2d([3, 3, 64, 128])),
+	'wfc1': tf.Variable(tf.contrib.layers.xavier_initializer([7*7*128, 1024])),
+	'wfc2': tf.Variable(tf.contrib.layers.xavier_initializer([1024, DIM_OUTPUT])),
 }
 
 # define the biases with respective shapes
@@ -68,19 +68,16 @@ def convolutional_neural_network(input):
 	layer_1_output = tf.nn.conv2d(input, weights['w1'], strides=[1, 1, 1, 1], padding='SAME') + biases['b1']
 	layer_1_output = tf.nn.relu(layer_1_output)
 	layer_1_output = tf.nn.max_pool(layer_1_output, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
-	layer_1_output = tf.nn.dropout(layer_1_output, 0.5)
 
 	layer_1_output = tf.contrib.layers.batch_norm(layer_1_output)
 	layer_2_output = tf.nn.conv2d(layer_1_output, weights['w2'], strides=[1, 1, 1, 1], padding='SAME') + biases['b2']
 	layer_2_output = tf.nn.relu(layer_2_output)
 	layer_2_output = tf.nn.max_pool(layer_2_output, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
-	layer_2_output = tf.nn.dropout(layer_2_output, 0.5)
 
 	layer_2_output = tf.contrib.layers.batch_norm(layer_2_output)
 	layer_3_output = tf.nn.conv2d(layer_2_output, weights['w3'], strides=[1, 1, 1, 1], padding='SAME') + biases['b3']
 	layer_3_output = tf.nn.relu(layer_3_output)
 	layer_3_output = tf.nn.max_pool(layer_3_output, ksize=[1, 2, 2, 1], strides=[1, 1, 1, 1], padding='SAME')
-	layer_3_output = tf.nn.dropout(layer_3_output, 0.5)
 
 	layer_3_output = tf.contrib.layers.batch_norm(layer_3_output)
 	layer_3_output = tf.reshape(layer_3_output, [-1, 7*7*128])	# flatten layer_4_output
